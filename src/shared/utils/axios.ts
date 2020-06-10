dee525408af6668b66b15b8a8a18518204589322
rawat07102 <ritikrawat0710@gmail.com>
2020-06-10 11:27:23 +0530

    added .env files for prod and dev with API_URL

--- src/shared/utils/axios.ts
+++ src/shared/utils/axios.ts
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  // baseURL: "https://nest-flix-server.herokuapp.com",
-  baseURL: "http://localhost:4000",
+  baseURL: process.env.API_URL,
   withCredentials: true,
 });
